ac3afe2437f74e833d771f69188a5e0cdf6fe212
SarahFrainer <sbfrainer@gmail.com>
2024-06-27 21:36:54 -0300

    ex-1

--- src/controllers/ClientsController.js
+++ src/controllers/ClientsController.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 
 const conexao = new Pool({
     host: 'localhost',
@@ -6,42 +6,41 @@ const conexao = new Pool({
     user: 'postgres',
     password: 'postgres',
     database: 'LabComerce'
-})
-
+});
 
 class ClientsController {
     async criar(request, response) {
-
         try {
             const dados = request.body;
 
+            // Verifique se todos os campos obrigatórios estão presentes
             if (!dados.nome || !dados.cpf || !dados.email || !dados.contact) {
                 return response.status(400).json({
                     mensagem: 'O nome, cpf, email e contact são obrigatórios'
-                })
+                });
             }
 
+            // Execute a query para inserir o cliente
             const client = await conexao.query(`
-            INSERT INTO clients
-             (nome,email,cpf,contact)
-             values
-             ($1,$2,$3,$4)
-             returning *
-        `, [dados.nome, dados.email, dados.cpf, dados.contact])
+                INSERT INTO clients
+                (nome, email, cpf, contact)
+                VALUES ($1, $2, $3, $4)
+                RETURNING *
+            `, [dados.nome, dados.email, dados.cpf, dados.contact]);
 
-            response.status(201).json(clients.rows[0])
+            // Verifique se foi inserido com sucesso
+            if (client.rows.length > 0) {
+                return response.status(201).json(client.rows[0]);
+            } else {
+                throw new Error('Falha ao inserir o cliente');
+            }
         } catch (error) {
-            response.status(500).json({
-                mensagem: 'Houve um erro ao cadastrar o serviço'
-            })
+            console.error('Erro ao cadastrar o cliente:', error.message);
+            return response.status(500).json({
+                mensagem: 'Houve um erro ao cadastrar o cliente'
+            });
         }
     }
-
-
 }
 
-
-
-
-
-module.exports = new ClientsController()
+module.exports = new ClientsController();
--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -5,8 +5,6 @@ const ClientsController = require('../controllers/ClientsController')
 const clientsRoutes = new Router()
 
 clientsRoutes.post('/', ClientsController.criar)
-clientsRoutes.get('/', ClientsController.listarTodos)
-clientsRoutes.get('/:id', ClientsController.listarUm)
-clientsRoutes.delete('/:id', ClientsController.deletar)
+
 
 module.exports = clientsRoutes;

bb44aec32b338d0e28f3193e1b0ebf38ca21d688
SarahFrainer <sbfrainer@gmail.com>
2024-06-27 21:14:17 -0300

    Ex-1

--- src/controllers/ClientsController.js
+++ src/controllers/ClientsController.js
@@ -5,11 +5,39 @@ const conexao = new Pool({
     port: 5432,
     user: 'postgres',
     password: 'postgres',
-    database: 'pets_bd'
+    database: 'LabComerce'
 })
 
 
 class ClientsController {
+    async criar(request, response) {
+
+        try {
+            const dados = request.body;
+
+            if (!dados.nome || !dados.cpf || !dados.email || !dados.contact) {
+                return response.status(400).json({
+                    mensagem: 'O nome, cpf, email e contact são obrigatórios'
+                })
+            }
+
+            const client = await conexao.query(`
+            INSERT INTO clients
+             (nome,email,cpf,contact)
+             values
+             ($1,$2,$3,$4)
+             returning *
+        `, [dados.nome, dados.email, dados.cpf, dados.contact])
+
+            response.status(201).json(clients.rows[0])
+        } catch (error) {
+            response.status(500).json({
+                mensagem: 'Houve um erro ao cadastrar o serviço'
+            })
+        }
+    }
+
+
 }
 
 
